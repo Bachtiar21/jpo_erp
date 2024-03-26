68895a10316b87d602a41094f30b5c249d29ee31
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-03-26 20:37:43 +0700

    update create SO with conditional broker

--- static/js/store/sales_create_so_create.js
+++ static/js/store/sales_create_so_create.js
@@ -7,9 +7,7 @@ const AllWarehouseByToken = BaseUrl + UrlGetWarehouseByToken;
 const GetAllBroker = BaseUrl + UrlGetAllContact;
 const PostSalesOrder = BaseUrl + UrlPostSalesOrder;
 
-// Fetch Data Kontak di Dropdown
 const dropdownVendor = document.getElementById("listSKU");
-// Fetch data dari API
 fetch(GetAllSKU, requestOptionsGet)
     .then((response) => response.json())
     .then((data) => {
@@ -25,7 +23,6 @@ fetch(GetAllSKU, requestOptionsGet)
 });
 
 const dropdownContact = document.getElementById("listContact");
-
 fetch(GetAllContact, requestOptionsGet)
     .then((response) => response.json())
     .then((data) => {
@@ -40,7 +37,6 @@ fetch(GetAllContact, requestOptionsGet)
         console.error('Error fetching SKU:', error);
 });
 
-// Fetch Data Broker di Dropdown
 const dropdownBroker = document.getElementById('listBroker')
 fetch(GetAllBroker, requestOptionsGet)
     .then((response) => response.json())
@@ -57,7 +53,6 @@ fetch(GetAllBroker, requestOptionsGet)
 });
 
 const dropdownWarehouse = document.getElementById("listWarehouse");
-// Fetch data dari API
 fetch(AllWarehouseByToken, requestOptionsGet)
     .then((response) => response.json())
     .then((data) => {
@@ -72,7 +67,6 @@ fetch(AllWarehouseByToken, requestOptionsGet)
         console.error('Error fetching warehouse:', error);
 });
 
-// Pengkondisian Radiobutton untuk Broker
 const radioButtons = document.querySelectorAll('input[name="radios-example"]');
 const brokerDiv = document.querySelector('.broker');
 const manualFee = document.querySelector('.manualFeeInput');
@@ -97,14 +91,16 @@ radioButtons.forEach(radioButton => {
             brokerDiv.style.display = 'block';
         } else {
             brokerDiv.style.display = 'none';
+            // Set broker and broker fee to null
+            document.querySelector('#listBroker').value = ""; 
+            document.querySelector('#manualFeeInput').value = "";
+            document.querySelector('#percenFeeInput').value = "";
         }
     });
 });
 
-// Event listener for the "Tambah SO" button
 const submitButton = document.querySelector('#submitButton');
 submitButton.addEventListener('click', () => {
-    // Get input values
     const listContact = document.querySelector('#listContact').value;
     const listWarehouse = document.querySelector('#listWarehouse').value;
     const listSKU = document.querySelector('#listSKU').value;
@@ -113,9 +109,8 @@ submitButton.addEventListener('click', () => {
     const stokKg = document.querySelector('#stokKg').value;
     const stokRib = document.querySelector('#stokRib').value;
     const hargaJualInput = document.querySelector('#hargaJualInput').value;
-    let listBroker = document.querySelector('#listBroker').value; // Gunakan let karena nilai listBroker akan diubah
+    let listBroker = document.querySelector('#listBroker').value;
 
-    // Check if any of the fields is empty
     if (!listContact || !listWarehouse || !listSKU || !dateInput || !stokRoll || !stokKg || !stokRib || !hargaJualInput) {
         Swal.fire({
             icon: 'warning',
@@ -125,26 +120,34 @@ submitButton.addEventListener('click', () => {
         return;
     }
 
-    // Pengkondisian Jika Broker tidak dipilih
     if (listBroker === "Pilih Broker") {
         listBroker = "";
     }
 
-    // Get broker fee value based on the selected type
     let brokerFeeValue;
     const manualFeeInput = document.querySelector('#manualFeeInput');
     const percenFeeInput = document.querySelector('#percenFeeInput');
-    if (document.querySelector('input[name="inline-radios-example"]:checked').value === 'manualFee') {
-        brokerFeeValue = manualFeeInput.value;
+    const isFeeSelected = document.querySelector('input[name="inline-radios-example"]:checked');
+
+    if (isFeeSelected) {
+        if (isFeeSelected.value === "manualFee") {
+            brokerFeeValue = manualFeeInput.value;
+        } else if (isFeeSelected.value === "percenFee") {
+            const percentageFee = parseFloat(percenFeeInput.value) / 100; 
+            brokerFeeValue = hargaJualInput * percentageFee;
+        } else {
+            brokerFeeValue = "";
+        }
     } else {
-        const percentageFee = parseFloat(percenFeeInput.value) / 100; 
-        brokerFeeValue = hargaJualInput * percentageFee;
+        brokerFeeValue = "";
     }
+
+    const isBrokerSelected = document.querySelector('input[name="radios-example"]:checked').value === "true";
 
     const postData = {
         sku: listSKU,
-        broker_fee: brokerFeeValue,
-        broker: listBroker,
+        broker_fee: isBrokerSelected ? brokerFeeValue : null,
+        broker: isBrokerSelected ? listBroker : null,
         contact_id: listContact,
         warehouse_id: listWarehouse,
         date: dateInput,
@@ -183,9 +186,6 @@ submitButton.addEventListener('click', () => {
                             timer: 1500,
                             showConfirmButton: false
                         })
-                        // .then(() => {
-                        //     window.location.href = 'sales_so_list_view.html';
-                        // });
                     } else {
                         Swal.fire({
                             icon: 'error',
@@ -196,7 +196,7 @@ submitButton.addEventListener('click', () => {
                 })
                 .catch(error => {
                     console.error("Error while adding SO data:", error);
-                });
+            });
         }
     });
 });
