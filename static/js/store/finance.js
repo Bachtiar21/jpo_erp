76d002d9dfef36d34d5a6b7e0c8da1ac4dfa6784
Bachtiar21 <bachtiarramadhan26@gmail.com>
2024-04-01 10:16:15 +0700

    hide code js untuk button create di finance

--- static/js/store/finance.js
+++ static/js/store/finance.js
@@ -3,41 +3,41 @@ import { CihuyDomReady, CihuyQuerySelector } from "https://c-craftjs.github.io/t
 import { BaseUrl, UrlGetAllCommission, UrlGetAllInvoice, UrlGetByIdContact, UrlGetAllBill, requestOptionsGet } from "../controller/template.js";
 import { getBadgePayment } from "../style/badge.js";
 
-document.addEventListener('DOMContentLoaded', function() {
-    const selectMenuFinance = document.getElementById('selectMenuFinance');
-    const invoiceMenu = document.getElementById('invoiceMenu');
-    const billMenu = document.getElementById('billMenu');
-    const comMenu = document.getElementById('comMenu');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const selectMenuFinance = document.getElementById('selectMenuFinance');
+//     const invoiceMenu = document.getElementById('invoiceMenu');
+//     const billMenu = document.getElementById('billMenu');
+//     const comMenu = document.getElementById('comMenu');
 
-    selectMenuFinance.addEventListener('change', function() {
-        if (selectMenuFinance.value === 'invoice') {
-            invoiceMenu.removeAttribute('hidden');
-            billMenu.setAttribute('hidden', true);
-            comMenu.setAttribute('hidden', true);
+//     selectMenuFinance.addEventListener('change', function() {
+//         if (selectMenuFinance.value === 'invoice') {
+//             invoiceMenu.removeAttribute('hidden');
+//             billMenu.setAttribute('hidden', true);
+//             comMenu.setAttribute('hidden', true);
 
-            // Ubah href button "Tambah Data" untuk Bill
-            document.getElementById('createButton').href = 'finance/finance_inv_create.html';
-        } else if (selectMenuFinance.value === 'bill') {
-            billMenu.removeAttribute('hidden');
-            invoiceMenu.setAttribute('hidden', true);
-            comMenu.setAttribute('hidden', true);
+//             // Ubah href button "Tambah Data" untuk Bill
+//             document.getElementById('createButton').href = 'finance/finance_inv_create.html';
+//         } else if (selectMenuFinance.value === 'bill') {
+//             billMenu.removeAttribute('hidden');
+//             invoiceMenu.setAttribute('hidden', true);
+//             comMenu.setAttribute('hidden', true);
 
-            // Ubah href button "Tambah Data" untuk Invoice
-            document.getElementById('createButton').href = 'finance/finance_bill_create.html';
-        } else if (selectMenuFinance.value === 'commision') {
-            comMenu.removeAttribute('hidden');
-            invoiceMenu.setAttribute('hidden', true);
-            billMenu.setAttribute('hidden', true);
+//             // Ubah href button "Tambah Data" untuk Invoice
+//             document.getElementById('createButton').href = 'finance/finance_bill_create.html';
+//         } else if (selectMenuFinance.value === 'commision') {
+//             comMenu.removeAttribute('hidden');
+//             invoiceMenu.setAttribute('hidden', true);
+//             billMenu.setAttribute('hidden', true);
 
-            // Ubah href button "Tambah Data" untuk Commision
-            document.getElementById('createButton').href = 'finance/finance_com_create.html';
-        } else {
-            invoiceMenu.setAttribute('hidden', true);
-            billMenu.setAttribute('hidden', true);
-            comMenu.setAttribute('hidden', true);
-        }
-    });
-});
+//             // Ubah href button "Tambah Data" untuk Commision
+//             document.getElementById('createButton').href = 'finance/finance_com_create.html';
+//         } else {
+//             invoiceMenu.setAttribute('hidden', true);
+//             billMenu.setAttribute('hidden', true);
+//             comMenu.setAttribute('hidden', true);
+//         }
+//     });
+// });
 
 // Fetch Data Invoices
 // Untuk Membuat Pagination
